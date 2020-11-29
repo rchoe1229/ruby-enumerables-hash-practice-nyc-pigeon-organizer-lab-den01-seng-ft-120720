dc6f5bd2a056a74ceb8c84b593a2fb3cb8cc845d
Ryan Choe <ryan.choe@outlook.com>
2020-11-29 22:06:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,11 +8,11 @@ def nyc_pigeon_organizer(data)
           final_array[name] = {}
         end
         if !final_array[name][key] 
-          final_array[name][key] = []
+          !final_array[name][key] = []
         end
+        final_array[name][key] << inner_key.to_s 
       end
-      final_array[name][key] << inner_key.to_s 
+      final_array
     end
-    final_array
   end
 end
