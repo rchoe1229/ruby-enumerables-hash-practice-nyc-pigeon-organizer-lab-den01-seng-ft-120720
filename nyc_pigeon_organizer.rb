5fb5d299f588e60fa013bc0a0c68870a28ee7e01
Ryan Choe <ryan.choe@outlook.com>
2020-11-29 22:00:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,10 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
-          final_array << name
+          final_array[name] = {}
+        end
+        if 
+      end
     end
   end
-end
 end
