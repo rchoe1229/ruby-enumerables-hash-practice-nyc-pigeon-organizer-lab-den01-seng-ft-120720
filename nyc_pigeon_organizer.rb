f37d79c2e5d7d57049b42d5b3a06c9b2871f2382
Ryan Choe <ryan.choe@outlook.com>
2020-11-29 21:44:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  organized_data = data.each_with_object({}) do |(key, value), array|
+  organized_data = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
     end
